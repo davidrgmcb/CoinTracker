7531f01f90fc5915cddfd93698d493063d57250f
bjneib <nei15004@byui.edu>
2019-06-05 19:31:19 -0600

    test

--- app/src/main/java/com/example/cointracker/MainActivity.java
+++ app/src/main/java/com/example/cointracker/MainActivity.java
@@ -53,5 +53,6 @@ public class MainActivity extends AppCompatActivity {
         //I know what I'm doing, trust me.
         //More conflicts more problems
     //NO Comments allowed
+    //comment
 }
 
