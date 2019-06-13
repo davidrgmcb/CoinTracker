72fd86126fa8fa58efd9e56e20770df188ae407d
davidrgmcb <davidrgmcburney@gmail.com>
2019-06-12 17:40:48 -0700

    Unit testing framework imported.

---
+++ app/src/test/java/com/example/cointracker/MainTest.java
@@ -0,0 +1,13 @@
+package com.example.cointracker;
+
+//import com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class MainTest {
+    @Test
+    public void assertEquality() {
+        assertEquals(4, 2+2);
+    }
+}
