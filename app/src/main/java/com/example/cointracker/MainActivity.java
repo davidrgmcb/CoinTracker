d721ba287c087b799af89246c0b1b06a82461c02
Clayton Thomas <onejay@outlook.com>
2019-06-14 20:20:52 -0700

    added demo button to main activity to toast data

--- app/src/main/java/com/example/cointracker/MainActivity.java
+++ app/src/main/java/com/example/cointracker/MainActivity.java
@@ -5,13 +5,17 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.gson.Gson;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    CryptoDataPoints[] listCDP;
 
     //This is a test to see if portfolio button works.
     public int portfolioButtonWorks = 0;
@@ -72,10 +76,26 @@ public class MainActivity extends AppCompatActivity {
             }
             System.out.println(cryptoResponse);
 
-            CryptoDataPoints[] cryptoDataPoints2 = new Gson().fromJson(cryptoResponse, CryptoDataPoints[].class);
-            System.out.println(cryptoDataPoints2[0].id + cryptoDataPoints2[0].current_price
-            + cryptoDataPoints2[1].id + cryptoDataPoints2[1].current_price);
+            final CryptoDataPoints[] listCDP = new Gson().fromJson(cryptoResponse, CryptoDataPoints[].class);
+            System.out.println(listCDP[0].id + listCDP[0].current_price
+                + listCDP[1].id + listCDP[1].current_price);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    saveListCDP(listCDP);
+                }
+            });
         }
+    }
+
+    void saveListCDP(CryptoDataPoints[] listCDP){
+        this.listCDP = listCDP;
+    }
+
+    public void DisplayCrypto(View view){
+        Toast.makeText(this, listCDP[0].id + listCDP[0].current_price
+                + listCDP[1].id + listCDP[1].current_price, Toast.LENGTH_LONG).show();
     }
 
 
