da068fcfd350759f9064ce47e026b7d78591d1a0
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-07-03 18:17:32 -0600

    modified navigation

--- app/src/main/java/com/example/cointracker/portfolio.java
+++ app/src/main/java/com/example/cointracker/portfolio.java
@@ -19,11 +19,11 @@ public class portfolio extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_portfolio:
+                    mTextMessage.setText(R.string.title_portfolio);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_all_crypto:
+                    mTextMessage.setText(R.string.title_all_crypto);
                     return true;
             }
             return false;
