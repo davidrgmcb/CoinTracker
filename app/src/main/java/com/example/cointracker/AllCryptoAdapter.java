16086f1e82a5576c069b964c83a7194a05b711c9
Clayton Thomas <onejay@outlook.com>
2019-07-06 19:13:21 -0700

    updates to all cryptos activity layout

--- app/src/main/java/com/example/cointracker/AllCryptoAdapter.java
+++ app/src/main/java/com/example/cointracker/AllCryptoAdapter.java
@@ -6,36 +6,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-//import com.example.android.common.logger.Log;
-
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-
-
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import java.util.LinkedList;
 
 public class AllCryptoAdapter extends RecyclerView.Adapter<AllCryptoAdapter.CryptoViewHolder> {
 
+
     class CryptoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        public final TextView cryptoItemView;
         final AllCryptoAdapter mAdapter;
+        public final TextView cryptoName;
+        TextView cryptoPrice;
+        TextView percentageChange;
+        TextView rank;
+
 
         public CryptoViewHolder(View itemView, AllCryptoAdapter adapter){
             super(itemView);
-            cryptoItemView = itemView.findViewById(R.id.cryptoItem);
+            cryptoName = itemView.findViewById(R.id.cryptoName);
+            cryptoPrice = itemView.findViewById(R.id.cryptoPrice);
+            percentageChange = itemView.findViewById(R.id.percentageChange);
+            rank = itemView.findViewById(R.id.rank);
             this.mAdapter = adapter;
             itemView.setOnClickListener(this);
         }
@@ -47,14 +38,15 @@ public class AllCryptoAdapter extends RecyclerView.Adapter<AllCryptoAdapter.Cryp
             // Use that to access the affected item in cryptoList.
             String element = cryptoList[mPosition].name; //get(mPosition);
             // Change the word in the mWordList.
-            cryptoList[mPosition].name = "Clicked + " + element; //set(mPosition, "Clicked! " + element);
+            //cryptoList[mPosition].name = "Clicked + " + element; //set(mPosition, "Clicked! " + element);
             // Notify the adapter, that the data has changed so it can
             // update the RecyclerView to display the data.
             mAdapter.notifyDataSetChanged();
         }
     }
 
-    //private final LinkedList<String> mWordList;
+
+    //dataset to be displayed
     private CryptoDataPoints[] cryptoList;
     private LayoutInflater mInflator;
 
@@ -72,8 +64,11 @@ public class AllCryptoAdapter extends RecyclerView.Adapter<AllCryptoAdapter.Cryp
 
     @Override
     public void onBindViewHolder(@NonNull AllCryptoAdapter.CryptoViewHolder holder, int position) {
-        String mCurrent = cryptoList[position].name; //get(position);
-        holder.cryptoItemView.setText(mCurrent);
+        CryptoDataPoints currentItem = cryptoList[position];
+        holder.cryptoName.setText(currentItem.name);//mCurrent);
+        holder.cryptoPrice.setText("$"+currentItem.current_price);
+        holder.percentageChange.setText(currentItem.price_change_percentage_24h + "%");
+        holder.rank.setText("#"+currentItem.market_cap_rank);
 
     }
 
@@ -85,82 +80,3 @@ public class AllCryptoAdapter extends RecyclerView.Adapter<AllCryptoAdapter.Cryp
 
 
 
-
-///////////////////////copied from RecyclerView sampler app///////////////////////
-/***************************************************************************
-
-  //Provide views to RecyclerView with data from mDataSet.
-
-public class AllCryptoAdapter extends RecyclerView.Adapter<AllCryptoAdapter.ViewHolder> {
-    private static final String TAG = "CustomAdapter";
-
-    private String[] mDataSet;
-
-    // BEGIN_INCLUDE(recyclerViewSampleViewHolder)
-
-     // Provide a reference to the type of views that you are using (custom ViewHolder)
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        //private final TextView textView;
-
-        public ViewHolder(View v) {
-            super(v);
-            // Define click listener for the ViewHolder's View.
-            v.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
-                }
-            });
-            //textView = (TextView) v.findViewById(R.id.textView);
-        }
-
-        //public TextView getTextView() {
-          //  return textView;
-        //}
-    }
-    // END_INCLUDE(recyclerViewSampleViewHolder)
-*********************************************************************/
-    /**
-     * Initialize the dataset of the Adapter.
-     *
-     * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
-     */
-/***************************************************************************
-
- public AllCryptoAdapter(String[] dataSet) {
-        mDataSet = dataSet;
-    }
-
-    // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
-    // Create new views (invoked by the layout manager)
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view.
-        View v = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.text_row_item, viewGroup, false);
-
-        return new ViewHolder(v);
-    }
-    // END_INCLUDE(recyclerViewOnCreateViewHolder)
-
-    // BEGIN_INCLUDE(recyclerViewOnBindViewHolder)
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        //Log.d(TAG, "Element " + position + " set.");
-
-        // Get element from your dataset at this position and replace the contents of the view
-        // with that element
-        viewHolder.getTextView().setText(mDataSet[position]);
-    }
-    // END_INCLUDE(recyclerViewOnBindViewHolder)
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataSet.length;
-    }
-}
-
-*********************************************************************/
